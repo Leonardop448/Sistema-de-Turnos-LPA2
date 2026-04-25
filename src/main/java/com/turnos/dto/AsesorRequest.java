f4e362cb7600e8d29068407fbd528e23e7f57550
atehojuan <atehojuan124@gmail.com>
2026-04-24 22:02:25 -0500

    Agrega validación @NotBlank al campo asesorId para asegurar que el ID del asesor sea obligatorio

--- src/main/java/com/turnos/dto/AsesorRequest.java
+++ src/main/java/com/turnos/dto/AsesorRequest.java
@@ -1,5 +1,6 @@
 package com.turnos.dto;
 
+import jakarta.validation.constraints.NotBlank;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -8,7 +9,12 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 @AllArgsConstructor
 public class AsesorRequest {
+
+    @NotBlank(message = "El ID del asesor es obligatorio")
     private String asesorId;
+
+    @NotBlank(message = "El nombre del asesor es obligatorio")
     private String nombre;
+
     private boolean disponible;
 }
